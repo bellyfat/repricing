8e2b37b7be02d415b36439b6157df14d507a83c2
johnson <englexu777@gmail.com>
2020-04-15 07:52:02 -0400

    Initial commit

--- crawl_tracking.py
+++ crawl_tracking.py
@@ -1,2 +1,19 @@
 import requests
-import beautifulsoup as bs4
+from bs4 import BeautifulSoup
+
+USPS_Hard_link = "https://tools.usps.com/go/TrackConfirmAction?tRef=fullpage&tLc=2&text28777=&tLabels="
+DHL_Hard_link = "https://www.dhl.com/global-en/home/tracking/tracking-ecommerce.html?submit=1&tracking-id="
+Fedex_Hard_link = "https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers="
+OSM_Hard_link = "https://www.osmworldwide.com/tracking/?trackingNumbers="
+UPS_Hard_link = "https://www.ups.com/track?loc=en_US&tracknum="
+# Other_Hard_link="https://www.fedex.com/apps/fedextrack/?action=track&tracknumbers="
+
+
+tracking_number = "9400111899561664217817"
+Base_url = USPS_Hard_link + tracking_number
+s = requests.Session()
+s.headers[
+    'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
+html = s.get(Base_url).text
+soup = BeautifulSoup(html, 'html.parser')
+print(soup)
